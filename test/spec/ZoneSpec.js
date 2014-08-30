b0f215b6af01bed8b72250cb290fa0d02912bbf6
Kosuke Nagano <gm.charlie@gmail.com>
2014-08-31 00:12:14 +0900

    modify to use Jasmine for test

---
+++ test/spec/ZoneSpec.js
@@ -0,0 +1,39 @@
+describe("Zone", function() {
+    var zone;
+    
+    beforeEach(function() {
+	zone = new Zone;
+    });
+    
+    it("enums", function() {
+	expect(zone.Front).toEqual("Front");
+	expect(zone.Middle).toEqual("Middle");
+	expect(zone.Right).toEqual("Right");
+	expect(zone.Left).toEqual("Left");
+	expect(zone.Rear).toEqual("Rear");
+	expect(zone.Center).toEqual("Center");
+    });
+    
+    describe("Functions", function() {
+	var driver;
+	var passernger;
+
+	beforeEach(function() {
+	    driver = zone.driver;
+	    passenger = zone.passenger;
+	});
+	
+	it("Equals", function() {
+	    console.log("hoge");
+            expect(driver.equals(zone.driver)).toEqual(true);
+	    console.log("fuga");
+            expect(driver.equals(passenger)).toEqual(false);
+	});
+	
+	it("Contains", function() {
+            expect(driver.contains(zone.driver)).toEqual(true);
+            expect(driver.contains(zone.passenger)).toEqual(false);
+	});
+    });
+    
+});
