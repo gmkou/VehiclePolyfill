b03ab4adc763d99a5751af88ed47b99263b3b478
Kosuke Nagano <gm.charlie@gmail.com>
2014-10-08 23:56:14 +0900

    modify indent; commentout unsupported test case

--- test/spec/VehicleSpeedSpec.js
+++ test/spec/VehicleSpeedSpec.js
@@ -1,39 +1,40 @@
 describe("VehicleSpeed", function(){
-    var vehicle;
-    var vehicleSpeed;
-    var vehicleSpeedSub;
+  var vehicle;
+  var retval;
+  
+  beforeEach(function(done){
+    vehicle = navigator.vehicle;
+    retval = null;
+    vehicle.vehicleSpeed.get().then(function(vs) {
+      setTimeout(function(){
+	retval = vs;
+	done();
+      }, 10); 
+    },function(error) {
+	setTimeout(function(){
+	  done();
+	}, 10); 
+      });
+  });
+  
+  it("vehicleSpeed.get()", function(done){
+    // @todo value "100" is temporal one
+    expect(retval.speed).toBe(100);
+    done();
+  });
 
-    beforeEach(function(done){
-	vehicle = navigator.vehicle;
-
-	vehicle.vehicleSpeed.get().then(function(vs) {
-	    setTimeout(function(){
-		vehicleSpeed = vs;
-		done();
-	    }, 10); 
-	},function(error) {
-	    setTimeout(function(){
-		done();
-	    }, 10); 
-	});
-    });
+/*
+  it("vehicleSpeed.subscribe()", function(done){
+    vehicleSpeedSub = vehicle.vehicleSpeed.subscribe(function(vehicleSpeed) {
+	                console.log("vehicle speed changed to: " + vehicleSpeed.speed);
+	                vehicle.vehicleSpeed.unsubscribe(vehicleSpeedSub);
+	              });
     
-    it("vehicleSpeed.get()", function(done){
-	// @todo value "100" is temporal one
-	expect(vehicleSpeed.speed).toBe(100);
-	done();
-    });
-
-    it("vehicleSpeed.subscribe()", function(done){
-	vehicleSpeedSub = vehicle.vehicleSpeed.subscribe(function(vehicleSpeed) {
-	    console.log("vehicle speed changed to: " + vehicleSpeed.speed);
-	    vehicle.vehicleSpeed.unsubscribe(vehicleSpeedSub);
-	});
-
-	expect(vehicleSpeedSub).not.toBe(0);
-	expect(vehicleSpeedSub).toHaveBeenCalled();
-
-	done();
-    });
+    expect(vehicleSpeedSub).not.toBe(0);
+    expect(vehicleSpeedSub).toHaveBeenCalled();
+    
+    done();
+  });
+*/
 
 });
